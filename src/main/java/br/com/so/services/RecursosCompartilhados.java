c39c80c73d6371fb42941d27e1dff9ca084a36d0
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-10-04 17:21:40 -0300

    Changes by alvaro

--- src/main/java/br/com/so/services/RecursosCompartilhados.java
+++ src/main/java/br/com/so/services/RecursosCompartilhados.java
@@ -1,16 +1,24 @@
 package br.com.so.services;
-
 import java.util.concurrent.locks.ReentrantLock;
 
 public class RecursosCompartilhados {
     private static int recursoA = 0;
     private static int recursoB = 0;
-
     private static ReentrantLock lockA = new ReentrantLock();
     private static ReentrantLock lockB = new ReentrantLock();
+    private static ReentrantLock lockPrint = new ReentrantLock();
+
+    private static void printSafe(String msg) {
+        lockPrint.lock();
+        try {
+            System.out.println(msg);
+        } finally {
+            lockPrint.unlock();
+        }
+    }
 
     public static void demonstrar() throws InterruptedException {
-        System.out.println("-> Primeiro: mostramos sem sincronização (corrida de dados) por alguns segundos");
+        printSafe("-> Primeiro: mostramos sem sincronização (corrida de dados) por alguns segundos");
         Thread[] semLock = new Thread[5];
         for (int i=0;i<5;i++) {
             final int id=i+1;
@@ -23,7 +31,7 @@ public class RecursosCompartilhados {
                     try { Thread.sleep(50); } catch (InterruptedException e) {}
                     recursoA = novaA;
                     recursoB = novaB;
-                    System.out.println("[SEM-LOCK] Thread " + id + " escreveu A=" + recursoA + " B=" + recursoB);
+                    printSafe("[SEM-LOCK] Thread " + id + " escreveu A=" + recursoA + " B=" + recursoB);
                     try { Thread.sleep(100); } catch (InterruptedException e) {}
                 }
             });
@@ -31,9 +39,10 @@ public class RecursosCompartilhados {
         }
         for (Thread t: semLock) t.join();
 
-        recursoA = 0; recursoB = 0;
+        recursoA = 0;
+        recursoB = 0;
 
-        System.out.println("\n-> Agora: mostramos com exclusão mútua usando locks. Cada thread " +
+        printSafe("\n-> Agora: mostramos com exclusão mútua usando locks. Cada thread " +
                 "tentará segurar ambos os recursos por 3 segundos (simulação)");
 
         Thread[] comLock = new Thread[5];
@@ -44,38 +53,44 @@ public class RecursosCompartilhados {
                     boolean acquired = false;
                     try {
                         lockA.lock();
-                        System.out.println("[LOCK] Thread " + id + " adquiriu lockA");
+                        printSafe("[LOCK] Thread " + id + " adquiriu lockA");
+
                         try { Thread.sleep(20); } catch (InterruptedException e) {}
+
                         lockB.lock();
-                        System.out.println("[LOCK] Thread " + id + " adquiriu lockB");
+                        printSafe("[LOCK] Thread " + id + " adquiriu lockB");
                         acquired = true;
 
                         recursoA += id;
                         recursoB += id*10;
-                        System.out.println("[CRITICO] Thread " + id + " consumindo recurso A=" + recursoA + " B=" + recursoB + " por 3s");
+                        printSafe("[CRITICO] Thread " + id + " consumindo recurso A=" + recursoA + " B=" + recursoB + " por 3s");
+
                         try { Thread.sleep(3000); } catch (InterruptedException e) {}
 
-                    } finally {
+                        // MUDANÇA: Prints ANTES de liberar os locks
+                        printSafe("[UNLOCK] Thread " + id + " liberou lockB");
+                        lockB.unlock();
+
+                        printSafe("[UNLOCK] Thread " + id + " liberou lockA");
+                        lockA.unlock();
+
+                    } catch (Exception e) {
                         if (acquired) {
                             lockB.unlock();
-                            System.out.println("[UNLOCK] Thread " + id + " liberou lockB");
                             lockA.unlock();
-                            System.out.println("[UNLOCK] Thread " + id + " liberou lockA");
                         } else {
                             if (lockA.isHeldByCurrentThread()) {
                                 lockA.unlock();
-                                System.out.println("[UNLOCK] Thread " + id + " liberou lockA (sem adquirir B)");
                             }
                         }
-                        try { Thread.sleep(200); } catch (InterruptedException e) {}
                     }
+                    try { Thread.sleep(200); } catch (InterruptedException e) {}
                 }
             });
             comLock[i].start();
             Thread.sleep(100);
         }
         for (Thread t: comLock) t.join();
-
-        System.out.println("Demonstração concluída.");
+        printSafe("Demonstração concluída.");
     }
 }
